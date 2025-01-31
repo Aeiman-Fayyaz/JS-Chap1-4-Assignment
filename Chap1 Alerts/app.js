c0a4dc7925f65e66835fc529793bae99cec9a951
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-01 01:10:56 +0500

    code update

---
+++ Chap1 Alerts/app.js
@@ -0,0 +1,57 @@
+// QUESTION1
+// Write a script to greet your website visitor using JS alert
+// box.
+
+// ANSWER
+/*alert ("Hello! Welcome on my website")*/
+
+
+// QUESTION2
+// Write a script to display following message on your web
+// page:
+
+// ANSWER
+/*alert ("Error! Please enter valid password")*/
+
+
+// QUESTION3
+// Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// ANSWER
+/*alert ("Welcome to my website \nHappy learning")*/
+
+
+// QUESTION4
+// Write a script to display following messages in sequence:
+
+// ANSWER
+/*alert ("Welcome back")
+alert ("Happy learning")*/
+
+
+// QUESTION5
+// Generate the following message through browser’s
+// developer console:
+
+// ANSWER
+/*alert ("Hello... I can run JS through my web browser's console")
+
+console.log("Hello... I can rus JS through my web browser's console");*/
+
+
+// QUESTION6
+// Make use of alerts in your new/existing HTML & CSS
+// project.
+
+// ANSWER
+/*alert ("New HTML file")*/
+
+
+// QUESTION7
+// Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
