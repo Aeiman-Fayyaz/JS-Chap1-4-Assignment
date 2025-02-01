e01dddd8e7fa07a245c8f11ad74af707d140df0c
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-02 00:02:33 +0500

    code update

---
+++ Chap 3 Variables for Numbers/app.js
@@ -0,0 +1,39 @@
+// QUESTION 1
+// Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// ANSWER
+/*let age = 23;
+
+alert (age)*/
+
+// QUESTION 2
+// Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// ANSWER
+
+
+
+// QUESTION 3
+// Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// ANSWER
+
+
+
+// QUESTION 4
+// A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// ANSWER
---
+++ Chap 4 Variable names- Legal and Illegal/app.js
@@ -0,0 +1,58 @@
+// QUESIION 1
+// Declare 3 variables in one statement.
+
+// ANSWER
+/*let city;
+
+let country;
+
+let state;*/
+
+// QUESTION 2
+// Declare 5 legal & 5 illegal variable names.
+
+// ANSWER
+// Legal Variables
+/*let userName;
+
+let user_Email;
+
+let $myName;
+
+let $fruits_Name;
+
+let _user;*/
+
+// Illegal Variables
+/*let bestemployeeoftheyear; (not all in small letters)
+
+let 3email; (doesnot start with numbers)
+
+let user*email; (special character are not allowed rather than 2 "_ ' $")
+
+let city; (same name variables not allowed)
+
+let city name; (spacing not allowed)*/
+
+
+// QUESTION 3
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// ANSWER
+/*document.write("<h1>Rules for naming JS variables</h1>");
+
+document.write(" b. userName, _userEmail, password, $user_1details")
+
+document.write(" c. small letter, _ or $, ")
+
+document.write(" d. camelCase")
+
+document.write(" e. same")*/
