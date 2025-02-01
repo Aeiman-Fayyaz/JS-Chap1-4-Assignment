0d80dc11b60cab841ba3fa7323b4b90984e2bbe8
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-01 19:11:57 +0500

    code update

--- Chap 2 Variables for Strings/app.js
+++ Chap 2 Variables for Strings/app.js
@@ -1,11 +1,11 @@
-// QUESTION1
+// QUESTION 1
 // Declare a variable called username.
 
 // ANSWER
 /*let userName;*/
 
 
-// QUESTION2
+// QUESTION 2
 // Declare a variable called myName & assign to it a string
 // that represents your Full Name.
 
@@ -15,13 +15,84 @@
 console.log(myName);*/
 
 
-// QUESTION3
+// QUESTION 3
 // Write script to
 // a) Declare a JS variable, titled message.
 // b) Assign “Hello World” to variable message
 // c) Display the message in alert box.
 
 // ANSWER
-/*let message = "Hello World"
+/*let message;
+
+message = "Hello World"
 
 alert (message)*/
+
+
+// QUESTION 4
+// Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+// ANSWER
+/*let studentName = "Aeiman Fayyaz"
+
+let studentAge = "23 years old"
+
+let studentClass = "Module2"
+
+alert(studentName)
+
+alert(studentAge)
+
+alert(studentClass)*/
+
+
+// QUESTION 5
+// Write a script to display the following alert using one JS
+// variable:
+
+// ANSWER
+/*let pizza = "PIZZA \nPIZZ \nPIZ \nPI\nP"
+
+alert(pizza)*/
+
+
+// QUESTION 6
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// ANSWER
+/*let email = "aeimanfayyaz2002@gmail.com"
+
+alert ("My email address is " + email)*/
+
+
+// QUESTION 7
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// ANSWER
+/*let book = "A smarter way to learn JavaScript"
+
+alert ("I am trying to learn from the book " + book)*/
+
+
+// QUESTION 8
+// Write a script to display this in browser through JS
+
+// ANSWER
+/*document.write("Yah! I can write HTML content through JavaScript")*/
+
+
+// QUESTION 9
+// Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+// ANSWER
+/*let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert (design)*/
